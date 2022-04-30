c66322dde09c6ab2a69403142d7ea589fb18c8b5
lukaofirst <lucasmacedo123@gmail.com>
2022-04-29 21:40:09 -0300

    Login Component

---
+++ src/components/Login.tsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { FormEvent } from 'react';
+import { useRef } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const Login = () => {
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const { signUp } = useAuth();
+
+    async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        try {
+            setError('');
+            setLoading(true);
+            await signUp(emailRef.current?.value!, passwordRef.current?.value!);
+        } catch (error) {
+            setError('Failed to create an account');
+            console.log(error);
+        }
+
+        setLoading(false);
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Log In</h2>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <Form onSubmit={(e) => handleSubmit(e)}>
+                        <Form.Group id='email' className='my-2'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type='email'
+                                ref={emailRef}
+                                required
+                            />
+                        </Form.Group>
+                        <Form.Group id='password' className='my-2'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control
+                                type='password'
+                                ref={passwordRef}
+                                required
+                            />
+                        </Form.Group>
+                        <Button
+                            disabled={loading}
+                            type='submit'
+                            className='w-100 my-4'
+                        >
+                            Log In
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                Need an Account? <Link to='/signup'>Sign Up</Link>
+            </div>
+        </>
+    );
+};
+
+export default Login;
