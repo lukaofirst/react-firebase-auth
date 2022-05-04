b443c73c3e25665fd20a775a3da750ae6f7c856a
lukaofirst <lucasmacedo123@gmail.com>
2022-05-03 22:59:19 -0300

    Dashboard Component

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,5 +1,38 @@
+import { useState } from 'react';
+import { Alert, Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    const [error, setError] = useState<string>('');
+
+    const { currentUser } = useAuth();
+
+    function handleLogout() {}
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Profile</h2>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <strong>Email: </strong>
+                    {currentUser.email}
+                    <Link
+                        to='/update-profile'
+                        className='btn btn-primary w-100 mt-3'
+                    >
+                        Update Profile
+                    </Link>
+                </Card.Body>
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                <Button variant='link' onClick={handleLogout}>
+                    Log Out
+                </Button>
+            </div>
+        </>
+    );
 };
 
 export default Dashboard;
